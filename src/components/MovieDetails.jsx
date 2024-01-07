e6bc02d7dbe1de3fcf43b6733c863d3d959033f5
Yassineafaila <yassineafaila55@gmail.com>
2024-01-07 23:03:27 +0000

    pref:refactor : add new structure

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -3,21 +3,24 @@ import { Link } from 'react-router-dom'
 import { IMG_URL } from '../constants'
 import { SIZE_IMG } from '../constants'
 import "./MovieDetails.scss"
-function MovieDetails({movie}) {
+function MovieDetails({ movie }) {
   return (
-    <Link className='hero_container'  to={"/movie/:id"}>
-                <img className='hero_images' src={`${IMG_URL}/${SIZE_IMG.backdrop.larger}${movie.backdrop_path}`} />
-                <div className='hero_content'>
-                    <h2>{movie.name ? movie.name : movie.title}</h2>
-                    <p>Type : {movie.media_type}</p>
-                    <p>
-                      {movie.first_air_date
-                        ? movie.first_air_date
-                        : movie.release_date}
-                    </p>
-                </div>
-              </Link>
-  )
+    <Link className="hero_container" to={`/movie/${movie.id}`}>
+      <div className="image_container">
+        <img
+          className="hero_image"
+          src={`${IMG_URL}/${SIZE_IMG.backdrop.larger}${movie.backdrop_path}`}
+        />
+      </div>
+      <div className="hero_content">
+        <h2>{movie.name ? movie.name : movie.title}</h2>
+        <p>Type: {movie.media_type}</p>
+        <p>
+          {movie.first_air_date ? movie.first_air_date : movie.release_date}
+        </p>
+      </div>
+    </Link>
+  );
 }
 
 export default MovieDetails
